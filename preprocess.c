3c8940a1bb040c9d194d4f19c70a7ee5bd0f9bf4
ILAYKID <ilaykid@gmail.com>
2023-03-20 23:38:24 +0200

    finish preprocess!!

--- preprocess.c
+++ preprocess.c
@@ -1,109 +1,120 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include "constants.h"
 
-typedef struct Macro {
-	char name[MAX_LABEL_LENGTH];
-	char** lines;
-	int num_lines;
-} Macro;
+typedef struct MacroLine {
+    char line[MAX_LINE_LENGTH];
+    struct MacroLine* next;
+} MacroLine;
 
-void insert_macro(const char* name, char** lines, int num_lines);
+typedef struct Macro {
+    char name[MAX_LABEL_LENGTH];
+    MacroLine* lines;
+    struct Macro* next;
+} Macro;
+static Macro* macro_table = NULL;
+void insert_macro(const char* name, MacroLine* lines);
 Macro* get_macro(const char* name);
 
-// Macro table
-static Macro* macro_table[MAX_MACROS];
-static int num_macros = 0;
-
 bool preprocess(const char* input_filename) {
-	FILE* input_file = fopen(input_filename, "r");
-	if (!input_file) {
-		printf("Error: Failed to open input file: %s\n", input_filename);
-		return false;
-	}
+    FILE* input_file = fopen(input_filename, "r");
+    if (!input_file) {
+        printf("Error: Failed to open input file: %s\n", input_filename);
+        return false;
+    }
+    char output_filename[MAX_FILENAME_LENGTH];
+    char base_input_filename[MAX_FILENAME_LENGTH];
+    int len1 = strlen(input_filename);
+    strncpy(base_input_filename, input_filename, len1 - 3);
+    base_input_filename[len1 - 3] = '\0';
+    sprintf(output_filename, "%s.am", base_input_filename);
+    FILE* output_file = fopen(output_filename, "w");
+    if (!output_file) {
+        printf("Error: Failed to create output file: %s\n", output_filename);
+        fclose(input_file);
+        return false;
+    }
 
-	char output_filename[MAX_FILENAME_LENGTH];
-	sprintf(output_filename, "%s.am", input_filename);
-	FILE* output_file = fopen(output_filename, "w");
-	if (!output_file) {
-		printf("Error: Failed to create output file: %s\n", output_filename);
-		fclose(input_file);
-		return false;
-	}
+    int macro_flag = 0;
+    char line[MAX_LINE_LENGTH];
+    char macro_name[MAX_LABEL_LENGTH];
 
-	int macro_flag = 0;
-	char line[MAX_LINE_LENGTH];
-	char macro_name[MAX_LABEL_LENGTH];
+    while (fgets(line, MAX_LINE_LENGTH, input_file)) {
+        /* Check for macro definition */
+        if (strncmp(line, "mcr", 3) == 0) {
+            /* Turn on macro flag */
+            macro_flag = 1;
+            /* Insert macro name into macro table */
+            sscanf(line, "mcr %s", macro_name);
+            MacroLine* lines = NULL;
+            MacroLine* curr_line = NULL;
+            while (fgets(line, MAX_LINE_LENGTH, input_file)) {
+                if (strncmp(line, "endmcr", 6) == 0) {
+                    /* Turn off macro flag */
+                    macro_flag = 0;
+                    /* Insert macro into macro table */
+                    insert_macro(macro_name, lines);
+                    break;
+                }
+                else {
+                    /* Add line to macro */
+                    MacroLine* new_line = (MacroLine*)malloc(sizeof(MacroLine));
+                    strncpy(new_line->line, line, MAX_LINE_LENGTH);
+                    new_line->next = NULL;
+                    if (!lines) {
+                        lines = new_line;
+                        curr_line = new_line;
+                    }
+                    else {
+                        curr_line->next = new_line;
+                        curr_line = new_line;
+                    }
+                }
+            }
+        }
+        else {
+            char macro_name[MAX_LABEL_LENGTH];
+            sscanf(line, "%s", macro_name);
+            Macro* macro = get_macro(macro_name);
+            if (macro) {
+                /* Copy macro lines to output file */
+                MacroLine* curr_line = macro->lines;
+                while (curr_line) {
+                    fprintf(output_file, "%s", curr_line->line);
+                    curr_line = curr_line->next;
+                }
+            }
+            else {
+                /* Write line to output file */
+                fputs(line, output_file);
+            }
+        }
+    }
 
-	while (fgets(line, MAX_LINE_LENGTH, input_file)) {
-		/* Check for macro definition */
-		if (strncmp(line, "mcr", 3) == 0) {
-			/* Turn on macro flag */
-			macro_flag = 1;
-			/* Insert macro name into macro table */
-			sscanf(line, "mcr %s", macro_name);
-			Macro* macro = ((Macro*)malloc(sizeof(Macro)));
-			strncpy(macro->name, macro_name, MAX_LABEL_LENGTH);
-			macro->num_lines = 156165;
-			macro->lines = malloc(macro->num_lines * sizeof(char*));
-			while (fgets(line, MAX_LINE_LENGTH, input_file)) {
-				if (strncmp(line, "endmcr", 6) == 0) {
-					/* Turn off macro flag */
-					macro_flag = 0;
-					/* Insert macro into macro table */
-					insert_macro(macro->name, macro->lines, macro->num_lines);
-					break;
-				}
-				else {
-					/* Add line to macro */
-					macro->lines[macro->num_lines] = malloc(MAX_LINE_LENGTH * sizeof(char));
-					strncpy(macro->lines[macro->num_lines], line, MAX_LINE_LENGTH);
-					macro->num_lines++;
-				}
-			}
-		}
-		else {
-			char macro_name[MAX_LABEL_LENGTH];
-			sscanf(line, "%s", macro_name);
-			Macro* macro = get_macro(macro_name);
-			if (macro) {
-				/* Copy macro lines to output file */
-				for (int i = 0; i < macro->num_lines; i++) {
-					fprintf(output_file, "%s", macro->lines[i]);
-				}
-			}
-			else {
-				/* Write line to output file */
-				fputs(line, output_file);
-			}
-		}
-	}
+    /* Close files */
+    fclose(input_file);
+    fclose(output_file);
 
-	/* Close files */
-	fclose(input_file);
-	fclose(output_file);
-
-	return true;
+    return true;
 }
 
-void insert_macro(const char* name, char** lines, int num_lines) {
-	if (num_macros == MAX_MACROS) {
-		printf("Error: Macro table is full\n");
-		return;
-	}
-	Macro* entry = (Macro*)malloc(sizeof(Macro));
-	strncpy(entry->name, name, MAX_LABEL_LENGTH);
-	entry->lines = lines;
-	entry->num_lines = num_lines;
-
-	macro_table[num_macros++] = entry;
+void insert_macro(const char* name, MacroLine* lines) {
+    Macro* new_macro = (Macro*)malloc(sizeof(Macro));
+    strncpy(new_macro->name, name, MAX_LABEL_LENGTH);
+    new_macro->lines = lines;
+    new_macro->next = macro_table;
+    macro_table = new_macro;
 }
+
 Macro* get_macro(const char* name) {
-	for (int i = 0; i < num_macros; i++) {
-		if (strcmp(macro_table[i]->name, name) == 0) {
-			return macro_table[i];
-		}
-	}
-	return NULL;
+    Macro* curr_macro = macro_table;
+    while (curr_macro) {
+        if (strcmp(curr_macro->name, name) == 0) {
+            return curr_macro;
+        }
+        curr_macro = curr_macro->next;
+    }
+    return NULL;
 }
